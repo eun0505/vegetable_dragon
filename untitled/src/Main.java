89fb0e8a4cd9c812f115d161994cc55d44f72056
Yongjae Kim <144149926+Songforthesilent@users.noreply.github.com>
2024-12-30 00:58:46 +0900

    Delete untitled directory

--- untitled/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("임시용");
-        System.out.println("피처 브랜치 탐색");
-    }
-}
