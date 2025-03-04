9d9b5a1bd44928a1811ffeb8aa48f1a06a6da51f
Chaewon Kim <rud15dns@naver.com>
2025-03-04 22:16:15 +0900

    feat : Connect news_ai_db to springboot by using JPA

--- src/main/java/com/example/vegetabledragon/VegetableDragonApplication.java
+++ src/main/java/com/example/vegetabledragon/VegetableDragonApplication.java
@@ -14,17 +14,17 @@ public class VegetableDragonApplication {
 
         // 🔹 MySQL 데이터베이스 연결 및 조회
         try {
-            String url = "jdbc:mysql://localhost:3306/vegetable";
+            String url = "jdbc:mysql://localhost:3306/news_ai_db";
             String UserName = "root";
             String Password = "1234";
 
             Connection connection = DriverManager.getConnection(url, UserName, Password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from user");
+            ResultSet resultSet = statement.executeQuery("select * from news_articles");
 
             if (resultSet.next()) {  // 🔹 여러 줄을 읽을 수 있도록 수정
-                String name = resultSet.getString("email");
-                System.out.println("사용자 이메일: " + name);
+//                String name = resultSet.getString("email");
+                System.out.println("무언가가 있습니다.");
             } else {
                 System.out.println("데이터가 없습니다.");
             }
--- src/main/java/com/example/vegetabledragon/repository/UserFeedbackRepository.java
+++ src/main/java/com/example/vegetabledragon/repository/UserFeedbackRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserFeedbackRepository extends JpaRepository<UserFeedback, Long> {
     
     // 특정 뉴스 게시글에 대해 피드백 개수를 세는 기능
-    long countByNewsArticleId_And_FeedbackType(Long newsId, UserFeedback.FeedbackType feedbackType);
+    long countByNewsArticleIdAndFeedbackType(Long newsArticleId, UserFeedback.FeedbackType feedbackType);
 }
